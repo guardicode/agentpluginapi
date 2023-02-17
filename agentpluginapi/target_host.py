1d7028398417e92c72c7492f9bc85585767e725f
Shreya Malviya <shreya.malviya@gmail.com>
2023-02-17 18:12:15 +0530

    Rename infection_monkey/model/host.py -> infection_monkey/i_puppet/target_host.py
    Original commit https://github.com/guardicore/monkey/commit/36ecd948cd1708737ed717b605c283ed2c66eb64

---
+++ agentpluginapi/target_host.py
@@ -0,0 +1,35 @@
+from ipaddress import IPv4Address
+from typing import Any, Dict, Optional, Union
+
+from pydantic import Field
+from typing_extensions import Literal  # import from `typing` once we switch to Python 3.8
+
+from common import OperatingSystem
+from common.base_models import MutableInfectionMonkeyBaseModel
+from common.types import NetworkPort, NetworkProtocol
+
+from . import PortScanData
+
+
+class TargetHost(MutableInfectionMonkeyBaseModel):
+    ip: IPv4Address
+    operating_system: Optional[OperatingSystem] = Field(default=None)
+    services: Dict[str, Any] = Field(default={})  # deprecated
+    icmp: bool = Field(default=False)
+    port_status: Dict[
+        Union[Literal[NetworkProtocol.TCP], Literal[NetworkProtocol.UDP]],
+        Dict[NetworkPort, PortScanData],
+    ] = Field(default={})
+
+    def __hash__(self):
+        return hash(self.ip)
+
+    def __str__(self):
+        victim = "Target Host %s: " % self.ip
+        if self.operating_system is not None:
+            victim += "OS - [ %s ]" % self.operating_system.value
+        victim += "Services - ["
+        for k, v in list(self.services.items()):
+            victim += "%s-%s " % (k, v)
+        victim += "] ICMP: %s " % (self.icmp)
+        return victim
