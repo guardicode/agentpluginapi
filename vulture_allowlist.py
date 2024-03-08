880e8e851ed2b7d066caa89c756393f4a8103d2b
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-03-08 09:15:12 -0500

    Add IAgentEventPublisher to vulture_allowlist.py

--- vulture_allowlist.py
+++ vulture_allowlist.py
@@ -6,6 +6,7 @@ from agentpluginapi import (
     FingerprintData,
     IAgentBinaryRepository,
     IAgentCommandBuilderFactory,
+    IAgentEventPublisher,
     IAgentOTPProvider,
     IHTTPAgentBinaryServerRegistrar,
     ILinuxAgentCommandBuilder,
@@ -28,6 +29,9 @@ from agentpluginapi import (
     WindowsRunOptions,
     WindowsShell,
 )
+
+IAgentEventPublisher.publish
+IAgentEventPublisher.event
 
 ITCPPortSelector
 ITCPPortSelector.get_free_tcp_port
